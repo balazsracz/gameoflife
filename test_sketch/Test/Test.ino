4427e132d79ec1aeae6bae7b2241438aaab13ce3
Balazs Racz <balazs.racz@gmail.com>
2023-07-10 21:02:34 +0200

    Adds example of how to use a hardware timer with the arduino API.

--- test_sketch/Test/Test.ino
+++ test_sketch/Test/Test.ino
@@ -8,28 +8,46 @@
   - Board = "generic STM32F0 series"
   - Board part number = "Generic STM32F072CBTx"
   - Upload method = "Stm32CubeProgrammer (DFU)"
+
+  To upload, press finger firmly on the FLASH pad, then use a metal coin two hit the RESET pad. Then click upload in the Arduino toolbar.
 */
 
-static constexpr auto LED13_PIN = PF0;
-static constexpr auto LED14_PIN = PF1;
+static constexpr int LED13_PIN = PF0;
+static constexpr int LED14_PIN = PF1;
 
-static constexpr auto MENU_PIN = PB15;
+static constexpr int MENU_PIN = PB15;
+
+#define BLINKER_TIMER TIM14
+
+HardwareTimer timCharlie(BLINKER_TIMER);
+
+void charliehandler(void) {
+  static int i = 0;
+  i++;
+  digitalWrite(LED14_PIN, (i%6) == 0);
+}
+
 
 void setup() {
   pinMode(LED13_PIN, OUTPUT);
   pinMode(LED14_PIN, OUTPUT);
   pinMode(MENU_PIN, INPUT_PULLUP);
+
+  timCharlie.attachInterrupt(charliehandler);
+  timCharlie.setMode(1, TIMER_DISABLED);
+  timCharlie.setOverflow(12, HERTZ_FORMAT);
+  timCharlie.resume();
 }
 
 void loop() {
   static int i = 0;
   i++;
   delay(1);
-  digitalWrite(LED14_PIN, 0);
+  //digitalWrite(LED14_PIN, 0);
   //digitalWrite(LED14_PIN, digitalRead(MENU_PIN));
   if (i % 1000 < 500) {
-    digitalWrite(LED13_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED13_PIN, HIGH);
   } else {
-    digitalWrite(LED13_PIN, LOW);   // turn the LED off by making the voltage LOW
+    digitalWrite(LED13_PIN, LOW);
   }
 }
