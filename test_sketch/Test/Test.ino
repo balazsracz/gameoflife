7268eaf994756e609abded05a09df5e3202fadc8
Balazs Racz <balazs.racz@gmail.com>
2023-07-17 10:32:20 +0200

    Adds implementation to read all touch pads.

--- test_sketch/Test/Test.ino
+++ test_sketch/Test/Test.ino
@@ -34,19 +34,145 @@ static constexpr int R6_PIN = PA8;
 
 
 TSC_HandleTypeDef TscHandle;
+TSC_IOConfigTypeDef IoConfig;
 
-bool startConv = false;
+bool start_conv = false;
 
 /* Array used to store the three acquisition value (one per channel) */
 __IO uint32_t uhTSCAcquisitionValue[3];
 
-uint8_t IdxBank = 0;
-TSC_IOConfigTypeDef IoConfig;
+// Stores the acquisition results. The lower these numbers are, the closer a finger is touching the respective buttons.
+// There are four rows and four columns. The first index selects rows vs columns, see kChRow, kChCol.
+uint32_t channel_results[2][4];
+// Use this constant in the first index of channel_results[][] to see the output for rows.
+static constexpr unsigned kChRow = 0;
+// Use this constant in the first index of channel_results[][] to see the output for columns.
+static constexpr unsigned kChCol = 1;
+
+struct AcquisitionPhase {
+  // Bitmask of which channel IOs to capture.
+  uint32_t channel_ios;
+  // Group2 channel: row or col.
+  uint8_t group2_rc;
+  // Group2 channel: which entry.
+  uint8_t group2_num;
+  // Group3 channel: row or col.
+  uint8_t group3_rc;
+  // Group3 channel: which entry.
+  uint8_t group3_num;
+  // Group5 channel: row or col.
+  uint8_t group5_rc;
+  // Group5 channel: which entry.
+  uint8_t group5_num;
+};
+
+static constexpr unsigned kNumPhases = 3;
+static const AcquisitionPhase kPhases[kNumPhases] = {
+  { TSC_GROUP2_IO2 | TSC_GROUP3_IO3 | TSC_GROUP5_IO2, kChCol, 0, kChRow, 1, kChCol, 2 },
+  { TSC_GROUP2_IO3 | TSC_GROUP3_IO4 | TSC_GROUP5_IO3, kChRow, 2, kChCol, 1, kChCol, 3 },
+  { TSC_GROUP2_IO4 | TSC_GROUP3_IO3 | TSC_GROUP5_IO4, kChRow, 0, kChRow, 1, kChRow, 3 }
+};
+// Which phase was last started.
+unsigned current_phase = 0;
 
 extern "C" {
   void TSC_IRQHandler(void) {
     HAL_TSC_IRQHandler(&TscHandle);
   }
+}
+
+void touch_setup() {
+  __HAL_RCC_TSC_CLK_ENABLE();
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /*##-2- Configure Sampling Capacitor IOs (Alternate-Function Open-Drain) ###*/
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+  GPIO_InitStruct.Alternate = GPIO_AF3_TSC;
+  // caps are A4 B0 B3
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_0;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*##-3- Configure Channel IOs (Alternate-Function Output PP) ###############*/
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+  // A 5-6-7
+  GPIO_InitStruct.Pin = GPIO_PIN_5;  // group2 io2
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_6;  // group2 io3
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_7;  // group2 io4
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // B 1-2 4-5-6
+  GPIO_InitStruct.Pin = GPIO_PIN_1;  // group3 io3
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_2;  // group3 io4
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_4;  // group5 io2
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  GPIO_InitStruct.Pin = GPIO_PIN_6;  // group5 io3
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  // B5 is incorrectly wired to a TCS circuit. B7 is a spare pin.
+  GPIO_InitStruct.Pin = GPIO_PIN_7;  // group5 io4
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  HAL_NVIC_SetPriority(TSC_IRQn, 1, 0);
+  HAL_NVIC_EnableIRQ(TSC_IRQn);
+
+  /*##-1- Configure the TSC peripheral #######################################*/
+  TscHandle.Instance = TSC;
+  TscHandle.Init.AcquisitionMode = TSC_ACQ_MODE_NORMAL;
+  TscHandle.Init.CTPulseHighLength = TSC_CTPH_1CYCLE;
+  TscHandle.Init.CTPulseLowLength = TSC_CTPL_1CYCLE;
+  TscHandle.Init.IODefaultMode = TSC_IODEF_IN_FLOAT; /* Because the electrodes are interlaced on this board */
+  TscHandle.Init.MaxCountInterrupt = ENABLE;
+  TscHandle.Init.MaxCountValue = TSC_MCV_16383;
+  TscHandle.Init.PulseGeneratorPrescaler = TSC_PG_PRESC_DIV64;
+  TscHandle.Init.SpreadSpectrum = DISABLE;
+  TscHandle.Init.SpreadSpectrumDeviation = 127;
+  TscHandle.Init.SpreadSpectrumPrescaler = TSC_SS_PRESC_DIV1;
+  TscHandle.Init.SynchroPinPolarity = TSC_SYNC_POLARITY_FALLING;
+  TscHandle.Init.ChannelIOs = 0;  /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
+  TscHandle.Init.SamplingIOs = 0; /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
+  TscHandle.Init.ShieldIOs = 0;   /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
+
+  if (HAL_TSC_Init(&TscHandle) != HAL_OK) {
+    /* Initialization Error */
+    Error_Handler();
+  }
+
+  // B5 we switch to analog mode.
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+
+void touch_iostart(unsigned phase) {
+  if (phase >= kNumPhases) {
+    phase = 0;
+  }
+  /*##-2- Configure the touch-sensing IOs ####################################*/
+  IoConfig.ChannelIOs = kPhases[phase].channel_ios;
+  IoConfig.SamplingIOs = TSC_GROUP2_IO1 | TSC_GROUP3_IO2 | TSC_GROUP5_IO1;
+  IoConfig.ShieldIOs = 0;
+
+  if (HAL_TSC_IOConfig(&TscHandle, &IoConfig) != HAL_OK) {
+    /* Initialization Error */
+    Error_Handler();
+  }
+  current_phase = phase;
+  //start_conv = false;
 }
 
 
@@ -91,6 +217,8 @@ void charliehandler(void) {
 }
 
 
+
+
 void setup() {
   pinMode(LED13_PIN, OUTPUT);
   pinMode(LED14_PIN, OUTPUT);
@@ -101,71 +229,8 @@ void setup() {
   timCharlie.setOverflow(6000, HERTZ_FORMAT);
   timCharlie.resume();
 
-  __HAL_RCC_TSC_CLK_ENABLE();
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  /*##-2- Configure Sampling Capacitor IOs (Alternate-Function Open-Drain) ###*/
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-  GPIO_InitStruct.Alternate = GPIO_AF3_TSC;
-  // caps are A4 B0 B3
-  GPIO_InitStruct.Pin = GPIO_PIN_4;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-  GPIO_InitStruct.Pin = GPIO_PIN_3;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*##-3- Configure Channel IOs (Alternate-Function Output PP) ###############*/
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-
-  GPIO_InitStruct.Pin = GPIO_PIN_5;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  GPIO_InitStruct.Pin = GPIO_PIN_7;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  HAL_NVIC_SetPriority(TSC_IRQn, 1, 0);
-  HAL_NVIC_EnableIRQ(TSC_IRQn);
-
-  /*##-1- Configure the TSC peripheral #######################################*/
-  TscHandle.Instance = TSC;
-  TscHandle.Init.AcquisitionMode = TSC_ACQ_MODE_NORMAL;
-  TscHandle.Init.CTPulseHighLength = TSC_CTPH_1CYCLE;
-  TscHandle.Init.CTPulseLowLength = TSC_CTPL_1CYCLE;
-  TscHandle.Init.IODefaultMode = TSC_IODEF_IN_FLOAT; /* Because the electrodes are interlaced on this board */
-  TscHandle.Init.MaxCountInterrupt = ENABLE;
-  TscHandle.Init.MaxCountValue = TSC_MCV_16383;
-  TscHandle.Init.PulseGeneratorPrescaler = TSC_PG_PRESC_DIV64;
-  TscHandle.Init.SpreadSpectrum = DISABLE;
-  TscHandle.Init.SpreadSpectrumDeviation = 127;
-  TscHandle.Init.SpreadSpectrumPrescaler = TSC_SS_PRESC_DIV1;
-  TscHandle.Init.SynchroPinPolarity = TSC_SYNC_POLARITY_FALLING;
-  TscHandle.Init.ChannelIOs = 0;  /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
-  TscHandle.Init.SamplingIOs = 0; /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
-  TscHandle.Init.ShieldIOs = 0;   /* Not needed yet. Will be set with HAL_TSC_IOConfig() */
-
-  if (HAL_TSC_Init(&TscHandle) != HAL_OK) {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-  /*##-2- Configure the touch-sensing IOs ####################################*/
-  IoConfig.ChannelIOs = TSC_GROUP2_IO2;  // | TSC_GROUP2_IO3 | TSC_GROUP2_IO4; /* Start with the first channel */
-  IoConfig.SamplingIOs = (TSC_GROUP2_IO1);
-  IoConfig.ShieldIOs = 0;
-
-  if (HAL_TSC_IOConfig(&TscHandle, &IoConfig) != HAL_OK) {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-  startConv = true;
+  touch_setup();
+  start_conv = true;
 }
 
 
@@ -176,90 +241,30 @@ void setup() {
   * @retval None
   */
 void HAL_TSC_ConvCpltCallback(TSC_HandleTypeDef* htsc) {
+  const AcquisitionPhase& cphase = kPhases[current_phase];
   /*##-5- Discharge the touch-sensing IOs ####################################*/
   HAL_TSC_IODischarge(&TscHandle, ENABLE);
   /* Note: a delay can be added here */
 
   /*##-6- Check if the First channel acquisition is correct (no max count) ###*/
   if (HAL_TSC_GroupGetStatus(&TscHandle, TSC_GROUP2_IDX) == TSC_GROUP_COMPLETED) {
-    /*##-7- Read the acquisition value #######################################*/
-    uhTSCAcquisitionValue[0] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP2_IDX);
-    startConv = true;
-    /* Note: Show the touch activity on LEDs.
-       The threshold values are indicative and may need to be adjusted */
-    if ((uhTSCAcquisitionValue[0] > 1000) && (uhTSCAcquisitionValue[0] < 1000))  // Channel 1
-    {
-    } else {
-    }
+    channel_results[cphase.group2_rc][cphase.group2_num] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP2_IDX);
+  } else {
+    channel_results[cphase.group2_rc][cphase.group2_num] = 1;
   }
 
-#if 0
-  /*##-8- Check if the Second channel acquisition is correct (no max count) ##*/
-  if (HAL_TSC_GroupGetStatus(&TscHandle, TSC_GROUP2_IDX) == TSC_GROUP_COMPLETED)
-  {
-    /*##-9- Read the acquisition value #######################################*/
-    uhTSCAcquisitionValue[IdxBank] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP2_IDX);  
-    /* Note: Show the touch activity on LEDs.
-       The threshold values are indicative and may need to be adjusted */
-    if ((uhTSCAcquisitionValue[1] > 1000) && (uhTSCAcquisitionValue[1] < TSCx_TS2_MAXTHRESHOLD)) // Channel 2
-    {
-      BSP_LED_On(LED6);
-    }
-    else
-    {
-      BSP_LED_Off(LED6);
-    }
+  if (HAL_TSC_GroupGetStatus(&TscHandle, TSC_GROUP3_IDX) == TSC_GROUP_COMPLETED) {
+    channel_results[cphase.group3_rc][cphase.group3_num] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP3_IDX);
+  } else {
+    channel_results[cphase.group3_rc][cphase.group3_num] = 1;
   }
 
-  /*##-10- Check if the Third channel acquisition is correct (no max count) ###*/
-  if (HAL_TSC_GroupGetStatus(&TscHandle, TSC_GROUP3_IDX) == TSC_GROUP_COMPLETED)
-  {
-    /*##-11- Read the acquisition value #######################################*/
-    uhTSCAcquisitionValue[IdxBank] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP3_IDX);  
-    /* Note: Show the touch activity on LEDs.
-       The threshold values are indicative and may need to be adjusted */
-    if ((uhTSCAcquisitionValue[2] > 1000) && (uhTSCAcquisitionValue[2] < TSCx_TS3_MAXTHRESHOLD)) // Channel 3
-    {
-      BSP_LED_On(LED5);
-    }
-    else
-    {
-      BSP_LED_Off(LED5);
-    }
+  if (HAL_TSC_GroupGetStatus(&TscHandle, TSC_GROUP5_IDX) == TSC_GROUP_COMPLETED) {
+    channel_results[cphase.group5_rc][cphase.group5_num] = HAL_TSC_GroupGetValue(&TscHandle, TSC_GROUP5_IDX);
+  } else {
+    channel_results[cphase.group5_rc][cphase.group5_num] = 1;
   }
-
-  /*##-12- Configure the next channels to be acquired #########################*/
-  switch (IdxBank)
-  {
-    case 0:
-      IoConfig.ChannelIOs = TSC_GROUP2_IO3; /* Second channel */
-      IdxBank = 1;
-      break;
-    case 1:
-      IoConfig.ChannelIOs = TSC_GROUP3_IO2; /* Third channel */
-      IdxBank = 2;
-      break;
-    case 2:
-      IoConfig.ChannelIOs = TSC_GROUP1_IO3; /* First channel */
-      IdxBank = 0;
-      break;
-    default:
-      break;
-  }
-#endif
-  if (HAL_TSC_IOConfig(&TscHandle, &IoConfig) != HAL_OK) {
-    /* Initialization Error */
-    Error_Handler();
-  }
-
-#if 0 
-  /*##-13- Re-start the acquisition process ###################################*/
-  if (HAL_TSC_Start_IT(&TscHandle) != HAL_OK)
-  {
-    /* Acquisition Error */
-    Error_Handler();
-  }
-#endif
+  start_conv = true;
 }
 
 void loop() {
@@ -267,7 +272,7 @@ void loop() {
   i++;
   delay(1);
 
-/*
+  /*
   pinMode(R3_PIN, INPUT);
   pinMode(R1_PIN, OUTPUT);
   pinMode(R2_PIN, OUTPUT);
@@ -280,18 +285,23 @@ void loop() {
   digitalWrite(R5_PIN, HIGH);
   */
 
-  if (startConv) {
-    static int kk = 0;
-    ++kk;
-    Serial.print("conv ");
-    Serial.print(kk);
-    Serial.print(" ");
-    Serial.println(uhTSCAcquisitionValue[0]);
-    startConv = false;
+  if (start_conv) {
+    start_conv = false;
+    if (current_phase == 0) {
+      static char buf[1000];
+      static int kk = 0;
+
+      snprintf(buf, sizeof(buf), "%d: R0 %d R1 %d R2 %d R3 %d| C0 %d C1 %d C2 %d C3 %d", kk, channel_results[0][0], channel_results[0][1], channel_results[0][2], channel_results[0][3],
+               channel_results[1][0], channel_results[1][1], channel_results[1][2], channel_results[1][3]);
+      ++kk;
+      while (Serial.availableForWrite() < strlen(buf) + 2);
+      Serial.println(buf);
+    }
     /*##-3- Discharge the touch-sensing IOs ####################################*/
     HAL_TSC_IODischarge(&TscHandle, ENABLE);
-    delay(1); /* 1 ms is more than enough to discharge all capacitors */
+    delay(10); /* 1 ms is more than enough to discharge all capacitors */
 
+    touch_iostart(++current_phase);
     /*##-4- Start the acquisition process ######################################*/
     if (HAL_TSC_Start_IT(&TscHandle) != HAL_OK) {
       /* Acquisition Error */
