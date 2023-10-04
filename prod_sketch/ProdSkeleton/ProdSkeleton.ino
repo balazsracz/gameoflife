3835ecc2f9ee33ae0161987d20a7e6e5bd61e557
Balazs Racz <balazs.racz@gmail.com>
2023-10-04 12:41:56 +0200

    Adds example that blinks the two LEDs.

--- prod_sketch/ProdSkeleton/ProdSkeleton.ino
+++ prod_sketch/ProdSkeleton/ProdSkeleton.ino
@@ -8,12 +8,20 @@ extern bool btn_row_active[4];
 // These variables are set to true when a given column in the key matrix is active (has a finger somewhere).
 extern bool btn_col_active[4];
 
+static constexpr int kLed13Pin = PF0;
+static constexpr int kLed14Pin = PF1;
+
+
 
 // This function will be called 6000 times per second by the timer.
 void Timer6000Hz() {
 }
 // This function will be called 4 times per second by the timer.
 void Timer4Hz() {
+  static int ctr = 0;
+  ++ctr;
+  digitalWrite(kLed13Pin, ctr & 1);
+  digitalWrite(kLed14Pin, !(ctr & 1));
 }
 
 
@@ -22,6 +30,9 @@ void setup() {
   // put your setup code here, to run once:
   TouchSetup();
   TimerSetup();
+
+  pinMode(kLed13Pin, OUTPUT);
+  pinMode(kLed14Pin, OUTPUT);
 }
 
 void loop() {
