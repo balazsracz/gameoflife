faabcce70ee5d8752dab31dcfa8bf8788f06294e
Balazs Racz <balazs.racz@gmail.com>
2023-10-13 10:59:56 +0200

    Switches over the ID program code to work independently of USB and faster.

--- prod_sketch/ProgramId/ProgramId.ino
+++ prod_sketch/ProgramId/ProgramId.ino
@@ -1,3 +1,11 @@
+// This is the address to program.
+static constexpr uint8_t kLo = 4;
+static constexpr uint8_t kHi = 0x85;
+
+// These two pins have an LED on them. The LED will light when you write LOW to the respective pin.
+static constexpr int kLed13Pin = PF0;
+static constexpr int kLed14Pin = PF1;
+
 
 static constexpr uint32_t kOBHiAddress = 0x1FFFF806;
 static constexpr uint32_t kOBLoAddress = 0x1FFFF804;
@@ -5,7 +13,7 @@ static constexpr uint32_t kOBLoAddress = 0x1FFFF804;
 
 void print_ob() {
   uint32_t *p = (uint32_t *)0x1FFFF800;
-  SerialUSB.printf("OB: %08x %08x %08x %08x\n", p[0], p[1], p[2], p[3]);
+  Serial.printf("OB: %08x %08x %08x %08x\n", p[0], p[1], p[2], p[3]);
 }
 
 bool ProgramId(uint8_t hi, uint8_t lo) {
@@ -14,23 +22,27 @@ bool ProgramId(uint8_t hi, uint8_t lo) {
   uint8_t clo = plo[0];
   print_ob();
   if (clo != lo || chi != hi) {
-    SerialUSB.printf("Setting userdata\n");
+    Serial.printf("Setting userdata\n");
+    digitalWrite(kLed13Pin, LOW);
+
+#if 0
     for (unsigned i = 0; i < 10; i++) {
       delay(1000);
-      SerialUSB.printf("Setting userdata %d\n", i);
+      Serial.printf("Setting userdata %d\n", i);
       print_ob();
     }
+#endif
     HAL_FLASH_Unlock();
     HAL_FLASH_OB_Unlock();
     FLASH_OBProgramInitTypeDef d;
     memset(&d, 0, sizeof(d));
     if ((plo[1] & lo) != lo || (plo[3] & hi) != hi) {
       // need erase
-      SerialUSB.printf("need OB erase\n");
+      Serial.printf("need OB erase\n");
       HAL_FLASHEx_OBGetConfig(&d);
       auto st = HAL_FLASHEx_OBErase();
       if (st != HAL_OK) {
-        SerialUSB.printf("Failed OB erase: %d %x\n", st, HAL_FLASH_GetError());
+        Serial.printf("Failed OB erase: %d %x\n", st, HAL_FLASH_GetError());
         print_ob();
         return false;
       }
@@ -40,32 +52,38 @@ bool ProgramId(uint8_t hi, uint8_t lo) {
     d.DATAData = hi;
     auto st = HAL_FLASHEx_OBProgram(&d);
     if (st != HAL_OK) {
-      SerialUSB.printf("Failed OB program: %d %x\n", st, HAL_FLASH_GetError());
+      Serial.printf("Failed OB program: %d %x\n", st, HAL_FLASH_GetError());
     }
     d.OptionType = OPTIONBYTE_DATA;
     d.DATAAddress = kOBLoAddress;
     d.DATAData = lo;
     st = HAL_FLASHEx_OBProgram(&d);
     if (st != HAL_OK) {
-      SerialUSB.printf("Failed OB program2: %d %x\n", st, HAL_FLASH_GetError());
+      Serial.printf("Failed OB program2: %d %x\n", st, HAL_FLASH_GetError());
     }
-    SerialUSB.printf("Setting userdata DONE\n");
+    Serial.printf("Setting userdata DONE\n");
     print_ob();
     delay(10);
+    digitalWrite(kLed14Pin, LOW);
+    delay(1000);
+
     HAL_FLASH_OB_Launch();
     return true;
   } else {
-    SerialUSB.printf("already good\n");
+    Serial.printf("already good\n");
     return false;
   }
 }
 
 
 void setup() {
-  SerialUSB.begin(9600);
-  delay(25);
+  Serial.begin(115200);
+  pinMode(kLed13Pin, OUTPUT);
+  pinMode(kLed14Pin, OUTPUT);
+  digitalWrite(kLed13Pin, HIGH);
+  digitalWrite(kLed14Pin, HIGH);
   // put your setup code here, to run once:
-  ProgramId(0x33, 0x02);
+  ProgramId(kHi, kLo);
 }
 
 uint64_t nmranet_nodeid() {
@@ -73,7 +91,15 @@ uint64_t nmranet_nodeid() {
 }
 
 void loop() {
-  SerialUSB.printf("node id: 0x0501%08x\n", uint32_t(nmranet_nodeid() & 0xfffffffful));
+  //Serial.printf("node id: 0x0501%08x\n", uint32_t(nmranet_nodeid() & 0xfffffffful));
   print_ob();
-  delay(3000);
+  auto id = nmranet_nodeid();
+  if ((id & 0xff) == kLo && ((id >> 8) & 0xff) == kHi) {
+    digitalWrite(kLed14Pin, LOW);
+    delay(250);
+    digitalWrite(kLed14Pin, HIGH);
+    digitalWrite(kLed13Pin, LOW);
+    delay(250);
+    digitalWrite(kLed13Pin, HIGH);
+  }
 }
