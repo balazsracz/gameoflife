f2dbbdfd636b443f8bbeac39d5a19259aad9b3ba
Balazs Racz <balazs.racz@gmail.com>
2023-10-16 21:03:52 +0200

    Remove dead code.

--- prod_sketch/ProdSkeleton/ProdSkeleton.ino
+++ prod_sketch/ProdSkeleton/ProdSkeleton.ino
@@ -360,18 +360,9 @@ void loop() {
   TouchLoop();
   TimerLoop();
   GlobalBusLoop();
+  LocalBusLoop();
   engine.Loop();
 
   digitalWrite(kLed13Pin, !leds[13 - 1]);
   digitalWrite(kLed14Pin, !leds[14 - 1]);
-
-  //LocalBusSignal(kNorth, btn_row_active[0] && (btn_col_active[1] || btn_col_active[2]));
-  //LocalBusSignal(kSouth, btn_row_active[3] && (btn_col_active[1] || btn_col_active[2]));
-  //LocalBusSignal(kWest, btn_col_active[0] && (btn_row_active[1] || btn_row_active[2]));
-  //LocalBusSignal(kEast, btn_col_active[3] && (btn_row_active[1] || btn_row_active[2]));
-
-  //leds[4] = LocalBusIsActive(kWest);
-  //leds[11] = LocalBusIsActive(kEast);
-  //leds[1] = LocalBusIsActive(kNorth);
-  //leds[14] = LocalBusIsActive(kSouth);
 }
